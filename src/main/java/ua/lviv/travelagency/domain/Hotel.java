e3aaa717f2cca090fdd797ee8b7bbd0560a4ad99
TarasBeshlei <32955001+TarasBeshlei@users.noreply.github.com>
2020-05-16 20:53:55 +0300

    Added classes TravelAgency, Hotel, Calendar

---
+++ src/main/java/ua/lviv/travelagency/domain/Hotel.java
@@ -0,0 +1,71 @@
+package ua.lviv.travelagency.domain;
+
+public class Hotel {
+
+    private Integer id;
+    private String name;
+    private Integer rating;
+    private String country;
+    private String city;
+    private Integer roomCount;
+
+    public Hotel() {
+    }
+
+    public Hotel(Integer id, String name, Integer rating, String country, String city, Integer roomCount) {
+        this.id = id;
+        this.name = name;
+        this.rating = rating;
+        this.country = country;
+        this.city = city;
+        this.roomCount = roomCount;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getRating() {
+        return rating;
+    }
+
+    public void setRating(Integer rating) {
+        this.rating = rating;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public Integer getRoomCount() {
+        return roomCount;
+    }
+
+    public void setRoomCount(Integer roomCount) {
+        this.roomCount = roomCount;
+    }
+}
---
+++ src/main/java/ua/lviv/travelagency/domain/TravelAgency.java
@@ -0,0 +1,50 @@
+package ua.lviv.travelagency.domain;
+
+public class TravelAgency {
+    private int id;
+    private String name;
+    private String address;
+    private String email;
+
+    public TravelAgency() {
+    }
+
+    public TravelAgency(int id, String name, String address, String email) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
