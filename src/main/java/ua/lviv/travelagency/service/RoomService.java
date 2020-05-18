925de23edcebaa634414309da49c32e5443fa22c
Yurii Zhurakovskyi <yuriizhurakovskyi7@gmail.com>
2020-05-18 12:19:14 +0300

    UserService, RoomService and implementations added

---
+++ src/main/java/ua/lviv/travelagency/service/RoomService.java
@@ -0,0 +1,7 @@
+package ua.lviv.travelagency.service;
+
+import ua.lviv.travelagency.domain.Room;
+import ua.lviv.travelagency.shared.AbstractCRUD;
+
+public interface RoomService extends AbstractCRUD<Room> {
+}
