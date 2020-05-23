1c364e001499bace55373d8f37e358f937b31aba
Yurii Zhurakovskyi <yuriizhurakovskyi7@gmail.com>
2020-05-23 12:46:30 +0300

    Added search result page with an output of search

---
+++ src/main/java/ua/lviv/travelagency/servlet/ResultServlet.java
@@ -0,0 +1,21 @@
+package ua.lviv.travelagency.servlet;
+
+import ua.lviv.travelagency.domain.Hotel;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet("/result")
+public class ResultServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        List<Hotel> hotels = (List<Hotel>) req.getSession().getAttribute("hotels");
+        req.setAttribute("hotels", hotels);
+        req.getRequestDispatcher("result.jsp").forward(req, resp);
+    }
+}
